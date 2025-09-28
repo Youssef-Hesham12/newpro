1ed9c5e5d64914fab0ec06d00708233a7061b142
Youssef-Hesham123 <youssefhesham1route@gmail.com>
2025-09-29 00:00:26 +0300

    helllllllo

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function middleware(request:NextRequest){
     const token =
     request.cookies.get("next-auth.session-token")?.value ||
-    request.cookies.get("_Secure-next-auth.session-token")?.value;
+    request.cookies.get("__Secure-next-auth.session-token")?.value;
     if(token){
         return NextResponse.next();
     }else{
