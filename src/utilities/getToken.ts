8c5e0344e6f1d6be6a7f796c1eb9f249c7486aa7
Youssef-Hesham123 <youssefhesham1route@gmail.com>
2025-09-29 00:03:55 +0300

    ddddddd

--- src/utilities/getToken.ts
+++ src/utilities/getToken.ts
@@ -5,7 +5,7 @@ import { cookies } from "next/headers"
 export default async function getToken(){
     try{
      const decodedToken=(await cookies()).get(`next-auth.session-token`)?.value || (await
-        cookies()).get ("_Secure-next-auth.session-token")?.value;
+        cookies()).get ("__Secure-next-auth.session-token")?.value;
        
      if(!decodedToken){
         return null;
